28f01b366b64162877a88518abb6a06e19914c32
Victor Alan Hernandez Muñoz <148152560+HMVictorAlan@users.noreply.github.com>
2023-10-26 13:58:50 -0600

    Update contacto.py
    Cambio de color (grey)

--- views/contacto.py
+++ views/contacto.py
@@ -23,7 +23,7 @@ def load_view():
     with col1:
         st.image("assets/images/profile_pic.png", width=333)
     with col2:
-        st.markdown("<h1 style='text-align: center; color: #d4d4d4;'> Victor Alan Hernandez Muñoz </h1>",
+        st.markdown("<h1 style='text-align: center; color: grey'> Victor Alan Hernandez Muñoz </h1>",
                     unsafe_allow_html=True)
         st.markdown('##')
         st.write(Descripción)
